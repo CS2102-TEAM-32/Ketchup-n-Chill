03946cf64a8d78626ddf7d1da0e8ca4c4d8e145f
wailunlim <wailun@u.nus.edu>
2019-09-03 18:31:32 +0800

    Add a comment

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const pgp = require('pg-promise')();
 
 // temp connectionUrl
+// follow: "postgres://YourUserName:YourPassword@YourHost:5432/YourDatabase"
 const connectionUrl = "postgres://wailun:wailun@localhost:5432/wailun";
 const db = pgp(connectionUrl);
 
