3cc9ba3f23f6332dcef1944d0ad631467bb8b765
Wai Lun <wailun@u.nus.edu>
2019-09-24 05:19:00 +0800

    Add middleware functions

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var dinersRouter = require('./routes/diners');
@@ -18,6 +20,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  session({
+    store: new (require('connect-pg-simple')(session))(),
+    secret: 'my-secret',
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/diners', dinersRouter);
