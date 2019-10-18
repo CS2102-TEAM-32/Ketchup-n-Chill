89e7d7577c58497ea329c69b231f88407ea3d8a7
Wai Lun <wailun@u.nus.edu>
2019-10-19 06:56:53 +0800

    Separate functions for index route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var controller = require('../controllers/index.controller');
+
 router.get('/', function(req, res, next) {
   res.redirect('/home');
 });
 
-router.get('/home', function (req, res, next) {
-  res.render('home');
-})
+router.get('/home', controller.showHomePage);
 
 module.exports = router;
