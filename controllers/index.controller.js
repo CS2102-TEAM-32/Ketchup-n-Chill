73323ddb22dcba3c1b5b2c09acfb74b47969c2e2
kayheen <kayheen@u.nus.edu>
2019-10-19 23:02:08 +0800

    Added the SQL query for Top 3 Restaurants

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -2,7 +2,11 @@ const db = require('../db/index');
 
 exports.showHomePage = async (req, res, next) => {
   try {
-    res.render('home');
+    const restaurant = await db.any('SELECT rname, round(AVG(rating)) AS avg FROM ReserveTimeSlots GROUP BY rname ORDER BY AVG(rating) LIMIT 3');
+    res.render('home', {
+      title: 'Ketchup and Chill!',
+      restaurants: restaurant
+    });
   } catch (e) {
     next(e);
   }
