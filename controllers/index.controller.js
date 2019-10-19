34b82ef4ebbc666d7c9edbf63fe80bcce1bb2efe
kayheen <kayheen@u.nus.edu>
2019-10-19 23:10:34 +0800

    Added DESC order for avg(rating)

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -2,7 +2,7 @@ const db = require('../db/index');
 
 exports.showHomePage = async (req, res, next) => {
   try {
-    const restaurant = await db.any('SELECT rname, round(AVG(rating)) AS avg FROM ReserveTimeSlots GROUP BY rname ORDER BY AVG(rating) LIMIT 3');
+    const restaurant = await db.any('SELECT rname, round(AVG(rating)) AS avg FROM ReserveTimeSlots GROUP BY rname ORDER BY AVG(rating) DESC, rname LIMIT 3');
     res.render('home', {
       title: 'Ketchup and Chill!',
       restaurants: restaurant
