c7c50c3525686a3e7ffda89257dd8235d388da81
tehwenyi <teh.wenyi22@gmail.com>
2019-11-04 20:11:40 +0800

    voucher code is displayed in modal when incentive is redeemed

--- routes/diners.js
+++ routes/diners.js
@@ -26,6 +26,10 @@ router.get(
   common.ensureAuthenticatedDiner,
   controller.showVouchers
 );
+router.get(
+  '/redeem',
+  controller.redeemVoucher
+);
 router.get('/account', function (req, res, next) {
   const page = 'account/' + [req.user.uname];
   console.log(page);
